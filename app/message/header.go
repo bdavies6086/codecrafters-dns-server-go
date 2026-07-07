473cff6de1b49d483bfc5c9559242d23b23c2a15
Ben Davies <bdavies6086@hotmail.co.uk>
2026-07-07 09:36:19 +0100

    codecrafters submit [skip ci]

--- app/message/header.go
+++ app/message/header.go
@@ -29,26 +29,26 @@ func (h Header) Encode() []byte {
 	fmt.Printf("%b\n", b2)
 
 	b3 := boolToUint8(h.Query, 0)
-	b3 = b3 | (h.OpCode >> 1)
+	b3 = b3 | (h.OpCode << 1)
 	b3 = b3 | boolToUint8(h.AuthorativeAnswer, 5)
 	b3 = b3 | boolToUint8(h.Truncation, 6)
 	b3 = b3 | boolToUint8(h.RecursionDesired, 7)
 
 	b4 := boolToUint8(h.RecursionAvailable, 0)
-	b4 = b4 | (h.Reserved >> 1)
-	b4 = b4 | (h.ResponseCode >> 4)
+	b4 = b4 | (h.Reserved << 1)
+	b4 = b4 | (h.ResponseCode << 4)
 
 	b5 := uint8(h.QuestionCount)
-	b6 := uint8(h.QuestionCount << 8)
+	b6 := uint8(h.QuestionCount >> 8)
 
 	b7 := uint8(h.AnswerRecordCount)
-	b8 := uint8(h.AnswerRecordCount << 8)
+	b8 := uint8(h.AnswerRecordCount >> 8)
 
 	b9 := uint8(h.AuthorityRecordCount)
-	b10 := uint8(h.AuthorityRecordCount << 8)
+	b10 := uint8(h.AuthorityRecordCount >> 8)
 
 	b11 := uint8(h.AdditionalRecordCount)
-	b12 := uint8(h.AdditionalRecordCount << 8)
+	b12 := uint8(h.AdditionalRecordCount >> 8)
 
 	return append(headerBytes, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12)
 
